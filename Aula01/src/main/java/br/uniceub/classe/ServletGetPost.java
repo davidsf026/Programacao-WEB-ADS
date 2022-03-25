7de740c43152b8f87bc079ab5a678055448716a8
david.sf <david.sf@AN0101715066240.dinfor.uniceub>
2022-03-24 21:33:14 -0300

    Aula sobre o uso dos métodos get/post

---
+++ Aula01/src/main/java/br/uniceub/classe/ServletGetPost.java
@@ -0,0 +1,43 @@
+package br.uniceub.classe;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletGetPost
+ */
+@WebServlet("/ServletGetPost")
+public class ServletGetPost extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+    	PrintWriter saida = resp.getWriter();
+    	
+		saida.write("<HTML><BODY>Olá! <br>");
+		saida.write("Respota do método GET");
+		saida.write("</BODY></HTML>");
+		
+		saida.close();
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+    	PrintWriter saida = resp.getWriter();
+    	
+		saida.write("<HTML><BODY>Olá! <br>");
+		saida.write("Respota do método POST");
+		saida.write("</BODY></HTML>");
+		
+		saida.close();
+    }
+	
+}
